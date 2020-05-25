3bbe51a4211602bb6ffbb8e955a965f6b6255d00
Steve Peters <skpeters87@gmail.com>
2020-05-24 21:03:06 -0400

    Bugfix with deployment sqlite

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from db import db
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-
-PORT = os.environ.get('PORT', 5000)
 
 app = Flask(__name__, template_folder='../template')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data.db'
@@ -27,4 +23,4 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == "__main__":
-    app.run(port=PORT, debug=True)
+    app.run(port=5000, debug=True)
