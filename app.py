f1c5b422b4d3fae759d0328e22995f3c064a6e92
Steve Peters <skpeters87@gmail.com>
2020-05-24 21:55:25 -0400

    Fix sqlite url in app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -7,8 +9,8 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-app = Flask(__name__, template_folder='../template')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data.db'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.secret_key = os.environ['SECRET_KEY']
 app.secret_key = 'secret'
